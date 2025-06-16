5f6e5defa45be4457117d43cf94213fd3659e033
Brko0-0 <andrija.brkic2004@gmail.com>
2025-06-16 14:39:19 +0200

    Delete Datoteka.h

--- Datoteka.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATOTEKA_H
-#define DATOTEKA_H
-
-void spremiUDatoteku(const char* naziv);
-void ucitajIzDatoteke(const char* naziv);
-StatusKod kopirajDatoteku(const char* izvor, const char* odrediste);
-void testDatoteke(const char* naziv);
-
-#endif
